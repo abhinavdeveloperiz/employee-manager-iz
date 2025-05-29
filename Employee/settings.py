c9a5e067dbbde05f575a1fcd20bb1966d34aab5e
abhinavmabhi <dexmarketing192018@gmail.com>
2025-05-29 15:28:37 +0530

    commit

--- Employee/settings.py
+++ Employee/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h1r5dj=assyt+p9mwqyl&so9y*&txk$q=nvz9qcv2@3gxk1yza
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
